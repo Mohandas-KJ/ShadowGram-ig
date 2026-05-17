9a24a7cde15edb5c8d8a8aad8316279f71dfc8dd
Mohan Das <mohandaskj.11@gmail.com>
2026-05-17 20:01:59 +0530

    Feature: Download posts from url

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from shadowmodules.Banner import showBanner
+from shadowmodules.Downloader import download_from_url
 
 if "__main__" == __name__:
     showBanner()
+    url = input('Enter url: ')
+    download_from_url(url)
--- shadowmodules/Downloader.py
+++ shadowmodules/Downloader.py
@@ -1,5 +1,21 @@
 #imports
 import instaloader
+import os
 
 def download_from_url(url):
+
+    os.makedirs("downloads",exist_ok=True)
+
     L = instaloader.Instaloader()
+
+    #shortcode extraction
+    shortcode = url.split("/")[-2]
+
+    post = instaloader.Post.from_shortcode(
+        L.context,
+        shortcode
+    )
+
+    L.download_post(post,target="downloads")
+
+    print("Download Complete")
